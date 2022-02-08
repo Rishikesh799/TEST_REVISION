0aad705f5700bf1ff25538f384ad7bd4910fe455
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-08 21:48:29 +0530

    Update Test_6.py

--- Test_6.py
+++ Test_6.py
@@ -6,3 +6,13 @@ shuffle(c)
 print(c)
 ['pink', 'red', 'green', 'blue', 'black']
 -----------------------------------------------------------------------------------------
+#wap to get the diff between the two list
+l=[1,3,5,7,9]
+k=[1,2,4,6,7,8]
+def diff(x,y):
+    j=set(x)
+    i=set(y)
+    print(list(j.symmetric_difference(i)))
+diff(l,k)
+[2, 3, 4, 5, 6, 8, 9]
+------------------------------------------------------------------------------
