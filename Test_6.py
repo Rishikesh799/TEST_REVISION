530d60a7fef8683058c7ee09115a0a081c6ad52b
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-08 21:47:31 +0530

    Update and rename Test_6 to Test_6.py

---
+++ Test_6.py
@@ -0,0 +1,8 @@
+-----------------------------------------------------------------------------------
+#write apython program to shuffle and print a specified list
+from random import shuffle
+c=["red","pink","blue","green","black"]
+shuffle(c)
+print(c)
+['pink', 'red', 'green', 'blue', 'black']
+-----------------------------------------------------------------------------------------
