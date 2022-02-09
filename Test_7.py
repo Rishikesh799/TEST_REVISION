760211e3bd0b36b7e6bee9ece9bd9c3596bcd951
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-09 21:05:59 +0530

    Update Test_7.py

--- Test_7.py
+++ Test_7.py
@@ -2,4 +2,12 @@
 #WAP to square the number of list using the map function
 l=[2,5,6,4]
 print(list(map(lambda x:x**2,l)))
+[4, 25, 36, 16]
 -------------------------------------------------------------------------
+#WAP to covert the all the chr in uppercase and lowercase and eliminate duplicate letters from given sequece
+l=["a","b","e","f","A","i","O","u","a"]
+y=set(l)
+x=list(map(lambda x:(x.upper(),x.lower()),y))
+print(x)
+
+---------------------------------------------------------
