9923322208b55ad667ee7ccd0eb464a9e263fa93
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-09 21:05:06 +0530

    Create Test_7.py

---
+++ Test_7.py
@@ -0,0 +1,5 @@
+-------------------USE MAP FUNCTION-------------------------------------------
+#WAP to square the number of list using the map function
+l=[2,5,6,4]
+print(list(map(lambda x:x**2,l)))
+-------------------------------------------------------------------------
