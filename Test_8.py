2e79c9a047f639beb0452520e7a3230cb0827f58
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-10 22:39:08 +0530

    Update Test_8.py

--- Test_8.py
+++ Test_8.py
@@ -3,3 +3,8 @@ x=[("english",88),("science",90),("maths",97),("social sccience",82)]
 print(sorted(x,key=lambda x:x[1],reverse=False))
 [('social sccience', 82), ('english', 88), ('science', 90), ('maths', 97)]
 -----------------------------------------------------------------------------------------
+#wap to sort list of dict using lambda
+x=[{"make":"nokia","model":216,"color":"black"},{"make":"mimax","model":2,"color":"gold"},{"make":"samsung","model":7,"color":"blue"}]
+print(sorted(x,key=lambda x:x["model"]))
+[{'make': 'mimax', 'model': 2, 'color': 'gold'}, {'make': 'samsung', 'model': 7, 'color': 'blue'}, {'make': 'nokia', 'model': 216, 'color': 'black'}]
+--------------------------------------------------------------------------------------------
