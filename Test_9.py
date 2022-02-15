2820d917f8669a6d83818cd89f99ce28f0b1959a
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-15 20:38:59 +0530

    Update Test_9.py

--- Test_9.py
+++ Test_9.py
@@ -1,1 +1,14 @@
+#TO check given number is armstrong or not
 
+n=int(input("Enter the no to check armstrong :"))
+i=n
+s=0
+while i>0:
+    x=i%10
+    s=s+x**3
+    i=i//10
+if n == s:
+    print(n,"number is armstrong")
+else:
+    print(n,"not armstrong")
+    ----------------------------------------------------------------------
