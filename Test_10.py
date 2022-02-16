5d2dd7cdddb11e9a59fad8e10b8126f055a4e93a
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-16 20:55:43 +0530

    Create Test_10.py

---
+++ Test_10.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the number"))
+d=[]
+for i in range(1,n):
+    if n%i==0:
+        d.append(i)
+if n==sum(d):
+    print(n,"Perfect number")
+else:
+    print(n,"not perfect number")
