842d2c242d16889213db1b5cb51d489f5214c0c4
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-16 20:56:56 +0530

    Update Test_10.py

--- Test_10.py
+++ Test_10.py
@@ -9,3 +9,12 @@ if n==sum(d):
     print(n,"Perfect number")
 else:
     print(n,"not perfect number")
+--------------------------------------------------------------------------
+#Find list of divisor of given natural number.
+
+n=int(input("Enter the natural number:"))
+d=[]
+for i in range(1,n+1):
+    if n%i==0:
+        d.append(i)
+print("The divisor are,",d)
