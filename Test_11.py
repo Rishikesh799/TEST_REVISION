824a624fbd238d94b70edbe6e0d93a2450e4a005
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-17 20:24:13 +0530

    Create Test_11.py

---
+++ Test_11.py
@@ -0,0 +1,10 @@
+#check whether number falls in a given range by using function
+
+def ran(n):
+    if n in range(1,10):
+        print(n,"Is present in a range.")
+    else:
+        print(n,"Not in range.")
+k=int(input("Enter the number :"))
+ran(k)
+----------------------------------------------------------------------------------------------------
