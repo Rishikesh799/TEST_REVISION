98dc5f10eaebaf53eb05ff6588dcf1c7e4c575b0
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-17 20:24:57 +0530

    Update Test_11.py

--- Test_11.py
+++ Test_11.py
@@ -8,3 +8,18 @@ def ran(n):
 k=int(input("Enter the number :"))
 ran(k)
 ----------------------------------------------------------------------------------------------------
+#WAP fuction that accept a string and calculate the number of upper case letters and lower case letter.
+s=input("Enter the string:")
+
+def stru(z):
+    c=0
+    k=0
+    for i in s:
+        if i.isupper():
+            c=c+1
+        elif i.islower():
+            k=k+1
+    print("NO of upper case letter is,",c)
+    print("No of lower case letter is,",k)
+stru(s)
+------------------------------------------------------------------------------
