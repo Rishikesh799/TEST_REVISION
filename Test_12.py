59ba62fcaf7fba205bd95fbaccb4f2524861f316
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-22 23:22:56 +0530

    Update Test_12.py

--- Test_12.py
+++ Test_12.py
@@ -5,3 +5,15 @@ for i in x:
     d.append("".join(i))
 print(str("".join(d)))
 ---------------------------------------------------------------------
+##Python programs replace all chr of a list except the given chr.
+x=["G","F","G","I","S","E","S","T"]
+v=["*"]
+y=["G"]
+d=[]
+for i in x:
+    if i != "G":
+        d.append("*")
+    else:
+        d.append(i)
+print(d)
+------------------------------------------------------------------------------------
