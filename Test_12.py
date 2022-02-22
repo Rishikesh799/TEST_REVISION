e8d6208df7dfb77faa840a3f9cde3058a9ba22ad
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-22 23:22:14 +0530

    Create Test_12.py

---
+++ Test_12.py
@@ -0,0 +1,7 @@
+##Convert chr of matrix into string
+x=[["g","f","g"],["i","s"],["b","e","s","t"]]
+d=[]
+for i in x:
+    d.append("".join(i))
+print(str("".join(d)))
+---------------------------------------------------------------------
