04132a3d655257d700f562098f40548993bda28b
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-23 22:59:14 +0530

    Update Test_13.py

--- Test_13.py
+++ Test_13.py
@@ -1,1 +1,8 @@
-
+#Program to print duplicates from list of integers
+x=[10,20,30,20,20,30,40,50,-20,60,60,-20,-20]
+d=[]
+for i in x:
+    if x.count(i)>1:
+        d.append(i)
+print(list(set(d)))
+--------------------------------------------------------
